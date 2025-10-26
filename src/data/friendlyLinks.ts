9ff2c3b67e0600db09b39d8ddb9f5c33ae0d99fe
S2ltyF1sh <rojasgraves82@gmail.com>
2025-10-26 21:02:45 +0800

    fix() 修改了图片存储位置

--- src/data/friendlyLinks.ts
+++ src/data/friendlyLinks.ts
@@ -16,7 +16,7 @@ export const friendlyLinks: Friend[] = [
 	{
 		name: "S2ltyF1sh",
 		url: "https://github.com/S2ltyF1sh/Blog",
-		logo: "/src/assets/logos/S2ltyF1sh.png",
+		logo: "/logos/S2ltyF1sh.png",
 		description: "自己的博客",
 		target: "_blank",
 		rel: "noopener noreferrer",
@@ -29,7 +29,7 @@ export const friendlyLinks: Friend[] = [
 	{
 		name: "折乙大神",
 		url: "https://zheyi.in/",
-		logo: "/src/assets/logos/zheyi.png",
+		logo: "/logos/zheyi.png",
 		description: "前端引路人（确信",
 		target: "_blank",
 		rel: "noopener noreferrer",
@@ -42,7 +42,7 @@ export const friendlyLinks: Friend[] = [
 	{
 		name: "ggbond",
 		url: "https://blog.ggbond-2025.top/",
-		logo: "/src/assets/logos/ggbond.png",
+		logo: "/logos/ggbond.png",
 		description: "后端战友（猪猪侠",
 		target: "_blank",
 		rel: "noopener noreferrer",
